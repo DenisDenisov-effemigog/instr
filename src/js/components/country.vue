804661ddf928111577ea81d010186a72e44a276c
Denisov Denis <effemigog@yandex.ru>
2020-11-19 11:23:24 +0300

    country choice

--- src/js/components/country.vue
+++ src/js/components/country.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="country country-close">
         <div class="country__bg"></div>
-        <div class="country-wrapper">
+        <div class="country__wrapper">
             <div class="country-container">
                 <div class="country__choice">
                     <h2 class="country__choice-title">Выбор страны</h2>
                     <ul class="country__choice-list">
                         <li v-for="item in countryArr" :key=item class="country__choice-item">
-                            <a href="#" class="country__choice-link">
+                            <a @click="choiceCountry" href="#" class="country__choice-link">
                                 {{item}}
                             </a>
                         </li>
@@ -16,7 +16,7 @@
                 <div class="country__map">
                     <img src="./images/country/map.png" alt="" class="country__map-pic">
                 </div>
-                <div class="country__close">
+                <div @click="closeChoiceCountry" class="country__close">
                     <div class="country__close-text">Закрыть</div>
                     <div class="country__close-pic">
                         <img src="./images/country/icons/close.svg" alt="" class="country__close-pic-icon">
@@ -33,6 +33,26 @@ export default {
     data(){
         return{
             countryArr:['Белоруссия','Казахстан','Румыния','Болгария','Польша','США','Венгрия','Россия','Украина']
+        }
+    },
+    methods:{
+        closeChoiceCountry(){
+            let country = document.querySelector('.country')
+            let countryBg = country.firstElementChild
+            let countryWrap = countryBg.nextElementSibling
+            let topnavIcon = document.querySelector('.topnav__switch-pic-icon')
+            country.classList.toggle('country-close')
+            countryBg.classList.toggle('country__bg-open')
+            countryWrap.classList.toggle('country__wrapper-open')
+            topnavIcon.classList.toggle('topnav__switch-pic-icon_opened')
+        },
+        choiceCountry(e){
+            let a = e.target
+            let countryLinkActive = document.querySelectorAll('.country__choice-link-active')
+            if(countryLinkActive.length > 0 && a !== countryLinkActive[0])
+                countryLinkActive[0].classList.remove('country__choice-link-active')
+            a.classList.toggle('country__choice-link-active')
+            this.$eventBus.$emit("countryName", a.textContent)
         }
     }
 }
--- src/js/components/topnav/topnav-switch.vue
+++ src/js/components/topnav/topnav-switch.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="topnav__switch">
-            <img src="./images/header/icons/arrow.svg" alt="открыть" class="topnav__switch-icon">
-            <div class="topnav__switch-text">Toolsworld/Romania</div>
+            <div class="topnav__switch-pic">
+                <img src="./images/header/icons/arrow.svg" alt="открыть" class="topnav__switch-pic-icon">
+            </div>
+            <div @click="clickToCountry" class="topnav__switch-text">Toolsworld/{{country}}</div>
     </div>
 </template>
 
@@ -9,6 +11,30 @@
 import country from '../country.vue';
 
 export default {
-    name: 'topnav-switch'
+    name: 'topnav-switch',
+    data(){
+        return{
+            country: 'Румыния'
+            
+        }
+    },
+    methods:{
+        clickToCountry(){
+            let country = document.querySelector('.country')
+            let countryBg = country.firstElementChild
+            let countryWrap = countryBg.nextElementSibling
+            let topnavIcon = document.querySelector('.topnav__switch-pic-icon')
+            country.classList.toggle('country-close')
+            countryBg.classList.toggle('country__bg-open')
+            countryWrap.classList.toggle('country__wrapper-open')
+            topnavIcon.classList.toggle('topnav__switch-pic-icon_opened')
+        },
+        countryName(name){
+            this.country = name
+        }
+    },
+    created(){
+        this.$eventBus.$on('countryName', this.countryName)
+    }
 }
 </script>
