5e3e0eef02267870fbeabb98dc271de2d805d8cf
Denisov Denis <effemigog@yandex.ru>
2020-12-09 12:32:40 +0300

    fix slider-promo

--- src/js/components/main-page/slider-promo.vue
+++ src/js/components/main-page/slider-promo.vue
@@ -3,16 +3,16 @@
         <div class="slider-promo__title">Промо моменты</div>
         <agile :options="options">
             
-            <div @mousemove="openTooltip" @mouseleave="closeTooltip" class="slider-promo__slide" v-for="slide in promoBanners">
-                <a :href="slide.link" class="">
+            <div @click="openModal" class="slider-promo__slide" v-for="slide in promoBanners">
+                <div class="">
                     <img :src="slide.img" alt="">
-                </a>
-                <div class="slider-promo__modal">
+                </div>
+                <!-- <div class="slider-promo__modal">
                     <svg class="slider-promo__modal-icon">
                         <use :xlink:href="templatePath + 'images/sprite.svg#icons__link'"></use>
                     </svg>
                     <p @click="openModal" class="slider-promo__modal-text">Подробнее об акции</p>
-                </div>
+                </div> -->
             </div>
             
             <template slot="prevButton">
@@ -64,17 +64,6 @@
         methods: {
             openModal(){
                 this.$eventBus.$emit("openModal", 'promo')
-            },
-            openTooltip(e){
-                let elemPositionTop = e.target.getBoundingClientRect().top.toFixed(0)
-                let elemPositionLeft = e.target.getBoundingClientRect().left.toFixed(0)
-                var x = (e.pageX - elemPositionLeft).toFixed(0);
-                var y = (Number(e.pageY) - (Number(elemPositionTop) + Number(window.pageYOffset))).toFixed(0)
-                console.log(y)
-                console.log(x)
-            },
-            closeTooltip(){
-
             }
         }
     }
