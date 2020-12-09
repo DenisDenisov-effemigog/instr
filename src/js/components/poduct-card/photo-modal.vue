743db95ca671d84f9de8ff491fa03bfe2f6a737c
Liliya Denisko <denisko@instrumtorg.ru>
2020-12-09 13:26:16 +0300

    photo modal

--- src/js/components/poduct-card/photo-modal.vue
+++ src/js/components/poduct-card/photo-modal.vue
@@ -1,58 +1,6 @@
 <template>
     <div class="photo-modal">
-        <!--<agile 
-            class="photo-modal__main"
-            :options="optionsMain"
-            ref="main"
-            :as-nav-for="asNavForMain"
-        >
-            <div class="photo-modal__main_slide" 
-                 v-for="(slide, index) in productImages"
-                 :key="index"
-                 :class="`slide&#45;&#45;${index}`"
-            >
-                <img :src="slide.img" alt="">
-            </div>
-
-            <template slot="prevButton">
-                <svg>
-                    <use :xlink:href="templatePath + 'images/sprite.svg#arrows__arrow-left'"></use>
-                </svg>
-            </template>
-            <template slot="nextButton">
-                <svg>
-                    <use :xlink:href="templatePath + 'images/sprite.svg#arrows__arrow-right'"></use>
-                </svg>
-            </template>
-        </agile>
-        <agile 
-            class="photo-modal__prev"
-            :options="optionsPrev"
-            ref="prev"
-            :as-nav-for="asNavForPrevious"
-        >
-
-            <div class="photo-modal__prev_slide"
-                 v-for="(slide, index) in productImages"
-                 :key="index"
-                 :class="`slide&#45;&#45;${index}`"
-                 @click="$refs.prev.goTo(index)"
-            >
-                <img :src="slide.img" alt="">
-            </div>
-
-            <template slot="prevButton">
-                <svg>
-                    <use :xlink:href="templatePath + 'images/sprite.svg#arrows__arrow-left'"></use>
-                </svg>
-            </template>
-            <template slot="nextButton">
-                <svg>
-                    <use :xlink:href="templatePath + 'images/sprite.svg#arrows__arrow-right'"></use>
-                </svg>
-            </template>
-        </agile>-->
-
+        <div class="photo-modal__title">Просмотр фотографий</div>
         <VueSlickCarousel
             class="photo-modal__main"
             v-bind="settings"
@@ -74,37 +22,50 @@
                 </svg>
             </template>
         </VueSlickCarousel>
-        <VueSlickCarousel
-            class="photo-modal__previous"
-            v-bind="settingsForPrev"
-            ref="previous"
-            :asNavFor="$refs.main"
-        >
-            <template #prevArrow>
-                <svg class="photo-modal__previous_arrow photo-modal__previous_arrow--prev" viewBox="1 2 7 10">
-                    <use :xlink:href="templatePath + 'images/sprite.svg#arrows__arrow-left'"></use>
-                </svg>
-            </template>
-            <div class="photo-modal__previous_slide"
-                 v-for="productImage in productImages">
-                <img :src="productImage.img" alt="">
-            </div>
-            <template #nextArrow>
-                <svg class="photo-modal__previous_arrow photo-modal__previous_arrow--next" viewBox="0 1 6 11">
-                    <use :xlink:href="templatePath + 'images/sprite.svg#arrows__arrow-right'"></use>
-                </svg>
-            </template>
-        </VueSlickCarousel>
+        <div class="photo-modal__footer">
+            <VueSlickCarousel
+                class="photo-modal__previous"
+                v-bind="settingsForPrev"
+                ref="previous"
+                :asNavFor="$refs.main"
+            >
+                <template #prevArrow>
+                    <svg class="photo-modal__previous_arrow photo-modal__previous_arrow--prev" viewBox="1 2 7 10">
+                        <use :xlink:href="templatePath + 'images/sprite.svg#arrows__arrow-left'"></use>
+                    </svg>
+                </template>
+                <div class="photo-modal__previous_slide"
+                     v-for="productImage in productImages">
+                    <img :src="productImage.img" alt="">
+                </div>
+                <template #nextArrow>
+                    <svg class="photo-modal__previous_arrow photo-modal__previous_arrow--next" viewBox="0 1 6 11">
+                        <use :xlink:href="templatePath + 'images/sprite.svg#arrows__arrow-right'"></use>
+                    </svg>
+                </template>
+            </VueSlickCarousel>
+            <component is="media-button"
+                       :svg="'play-btn'"
+                       :action="'openVideo'"
+                       :link="'http://www.youtube.com/embed/ODEsC4Ijc2o'"
+            ></component>
+            <component is="media-button"
+                       :svg="'around-photo'"
+                       :action="'openVideo'"
+                       :link="'https://instrument.ru/info/3d_photo/14109/READY/3D.html'"
+            ></component>
+        </div>
     </div>
 </template>
 
 <script>
 import VueSlickCarousel from "vue-slick-carousel";
+import mediaButton from "../media-button.vue";
 
 export default {
     name: 'photo-modal',
     components: {
-        VueSlickCarousel
+        VueSlickCarousel, mediaButton
     },
     props: {
         productImages: {
