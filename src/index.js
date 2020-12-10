0bb5d6e57aa9f6ab4582f7a1c4ab86732f0704bd
Liliya Denisko <denisko@instrumtorg.ru>
2020-12-10 11:10:16 +0300

    added router, refactor components

--- src/index.js
+++ src/index.js
@@ -1,47 +1,19 @@
 import Vue from 'vue';
-
 import VueAgile from 'vue-agile';
 import VueSlickCarousel from 'vue-slick-carousel';
-
 import store from './js/store';
+import router from './js/router';
 import HelpersMixin from './js/mixins/helper';
-
-
-import country from '../src/js/components/country.vue';
-import modal from '../src/js/components/modal.vue';
-import headerBlock from '../src/js/components/header/header-block.vue';
-import stickyHeader from '../src/js/components/header/sticky-header.vue';
-import topnavSwitch from '../src/js/components/topnav/topnav-switch.vue';
-import sliderMain from '../src/js/components/main-page/slider-main.vue';
-import sliderPromo from '../src/js/components/main-page/slider-promo.vue';
-import sliderPromoModal from '../src/js/components/main-page/slider-promo-modal.vue';
-import sliderCards from '../src/js/components/main-page/slider-cards.vue';
-import card from '../src/js/components/card/card.vue';
-import sliderPhotoCard from './js/components/card/slider-photo-card.vue';
-import sliderPhotoDots from './js/components/card/slider-photo-dots.vue';
-import addToCart from './js/components/card/add-to-cart.vue';
-import homeMenu from './js/components/header/mobile/home-menu.vue';
-import productCardSlider from './js/components/poduct-card/product-card-slider.vue';
-import productTabs from './js/components/poduct-card/product-tabs.vue';
-import productTabsQuestions from './js/components/poduct-card/product-tabs-questions.vue';
-import stickyCard from './js/components/poduct-card/sticky-card.vue';
-import productInfoAll from './js/components/product-info/product-info-all.vue';
-import productCardArticle from './js/components/poduct-card/product-card-article.vue';
-import photoModal from './js/components/poduct-card/photo-modal.vue';
 
 Vue.use(VueAgile)
 Vue.mixin(HelpersMixin)
 
-import catalogue from '../src/js/components/catalogue/catalogue.vue';
-import mobileMenu from '../src/js/components/mobile-menu.vue';
-import stickyMenu from '../src/js/components/sticky-menu.vue';
-import stickyMobileButton from '../src/js/components/sticky-mobile-button.vue';
-import inFavorite from '../src/js/components/in-favorite.vue';
-import toCompare from '../src/js/components/to-compare.vue';
-import mediaButton from '../src/js/components/media-button.vue';
-import profile from '../src/js/components/profile/profile.vue';
-
 Vue.prototype.$eventBus = new Vue();
+
+const requireComponent = require('./js/components.js');
+for(let idx in requireComponent.default.components) {
+    Vue.component(requireComponent.default.components[idx].name, requireComponent.default.components[idx]);
+}
 
 Vue.directive('scroll', {
     inserted: function (el, binding) {
@@ -57,38 +29,7 @@ Vue.directive('scroll', {
 const app = new Vue({
     el: '#app',
     store,
-    components: {
-        country,
-        topnavSwitch,
-        headerBlock,
-        stickyHeader,
-        sliderMain,
-        sliderPromo,
-        sliderCards,
-        card,
-        sliderPhotoCard,
-        catalogue,
-        addToCart,
-        mobileMenu,
-        homeMenu,
-        stickyMenu,
-        modal,
-        stickyMobileButton,
-        productCardSlider,
-        productTabs,
-        productTabsQuestions,
-        VueSlickCarousel,
-        sliderPhotoDots,
-        productCardArticle,
-        photoModal,
-        productInfoAll,
-        stickyCard,
-        sliderPromoModal,
-        inFavorite,
-        toCompare,
-        mediaButton,
-        profile,
-    },
+    router,
     mounted() {
     }
 });
---
+++ src/js/components.js
@@ -0,0 +1,72 @@
+import VueSlickCarousel from "vue-slick-carousel";
+/*elements*/
+import country from './components/country.vue';
+import modal from './components/modal.vue';
+import mobileMenu from './components/mobile-menu.vue';
+import stickyMenu from './components/sticky-menu.vue';
+import stickyMobileButton from './components/sticky-mobile-button.vue';
+import inFavorite from './components/in-favorite.vue';
+import toCompare from './components/to-compare.vue';
+import mediaButton from './components/media-button.vue';
+/*catalogue*/
+import catalogue from './components/catalogue/catalogue.vue';
+/*header*/
+import headerBlock from './components/header/header-block.vue';
+import stickyHeader from './components/header/sticky-header.vue';
+import homeMenu from './components/header/mobile/home-menu.vue';
+import topnavSwitch from './components/topnav/topnav-switch.vue';
+/*main-page*/
+import sliderMain from './components/main-page/slider-main.vue';
+import sliderPromo from './components/main-page/slider-promo.vue';
+import sliderPromoModal from './components/main-page/slider-promo-modal.vue';
+import sliderCards from './components/main-page/slider-cards.vue';
+/*card*/
+import card from './components/card/card.vue';
+import sliderPhotoCard from './components/card/slider-photo-card.vue';
+import sliderPhotoDots from './components/card/slider-photo-dots.vue';
+import addToCart from './components/card/add-to-cart.vue';
+/*product-card*/
+import productCardSlider from './components/poduct-card/product-card-slider.vue';
+import productTabs from './components/poduct-card/product-tabs.vue';
+import productTabsQuestions from './components/poduct-card/product-tabs-questions.vue';
+import stickyCard from './components/poduct-card/sticky-card.vue';
+import productCardArticle from './components/poduct-card/product-card-article.vue';
+import photoModal from './components/poduct-card/photo-modal.vue';
+import productInfoAll from './components/poduct-card/product-info-all.vue';
+/*profile*/
+import profile from './components/profile/profile.vue';
+
+export default {
+    components: {
+        country,
+        topnavSwitch,
+        headerBlock,
+        stickyHeader,
+        sliderMain,
+        sliderPromo,
+        sliderCards,
+        card,
+        sliderPhotoCard,
+        catalogue,
+        addToCart,
+        mobileMenu,
+        homeMenu,
+        stickyMenu,
+        modal,
+        stickyMobileButton,
+        productCardSlider,
+        productTabs,
+        productTabsQuestions,
+        VueSlickCarousel,
+        sliderPhotoDots,
+        productCardArticle,
+        photoModal,
+        productInfoAll,
+        stickyCard,
+        sliderPromoModal,
+        inFavorite,
+        toCompare,
+        mediaButton,
+        profile,
+    },
+}
