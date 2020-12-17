3cbdc99806dd804ff34823893134e933d2d5c19c
Liliya Denisko <denisko@instrumtorg.ru>
2020-12-17 10:31:23 +0300

    fix sticky button

--- src/js/components/poduct-card/sticky-card.vue
+++ src/js/components/poduct-card/sticky-card.vue
@@ -1,41 +1,52 @@
 <template>
-    <div class="sticky-card" :class="{'sticky-card--fixed': flag}">
-        <div class="container">
-            <div class="sticky-card__img">
-                <img :src="product.images[0].img" alt="">
-            </div>
-            <div class="sticky-card__desc">
-                <div class="sticky-card__stickers">
-                    <div class="product-card__stickers_sticker-wrap"
-                    v-for="sticer in product.tooltips">
-                        <div class="product-card__stickers_sticker"
-                             :class="'product-card__stickers_sticker product-card__stickers_sticker--' + sticer.status">
-                            <span>{{ sticer.title }}</span>
+    <div>
+        <div class="sticky-card" :class="{'sticky-card--fixed': showStickyCard}">
+            <div class="container">
+                <div class="sticky-card__img">
+                    <img :src="product.images[0].img" alt="">
+                </div>
+                <div class="sticky-card__desc">
+                    <div class="sticky-card__stickers">
+                        <div class="product-card__stickers_sticker-wrap"
+                             v-for="sticer in product.tooltips">
+                            <div class="product-card__stickers_sticker"
+                                 :class="'product-card__stickers_sticker product-card__stickers_sticker--' + sticer.status">
+                                <span>{{ sticer.title }}</span>
+                            </div>
                         </div>
                     </div>
+                    <div class="sticky-card__text">{{ product.name }}</div>
                 </div>
-                <div class="sticky-card__text">{{ product.name }}</div>
+                <div class="sticky-card__price">
+                    <div v-for="price in product.prices"
+                         v-if="price.base === 1"
+                         class="sticky-card__price--old"
+                    >{{ price.price }} &#8381;</div>
+                    <div v-for="price in product.prices"
+                         v-if="price.base === 3"
+                         class="sticky-card__price--total"
+                    >{{ price.price }} &#8381;</div>
+                </div>
+                <component is="add-to-cart"
+                           :product-id="product.id"
+                           :max-amount="product.stock"
+                ></component>
             </div>
-            <div class="sticky-card__price">
-                <div v-for="price in product.prices"
-                     v-if="price.base === 1"
-                     class="sticky-card__price--old"
-                >{{ price.price }} &#8381;</div>
-                <div v-for="price in product.prices"
-                     v-if="price.base === 3"
-                     class="sticky-card__price--total"
-                >{{ price.price }} &#8381;</div>
-            </div>
-            <component is="add-to-cart"  
-                       :product-id="product.id"
-                       :max-amount="product.stock"
+        </div>
+        <div class="sticky-mobile-button" v-if="showButton">
+            <component is="add-to-cart"
+                       :text="'В корзине'"
+                       :button-title="'Добавить в корзину'"
+                       :size="'big'"
+                       :productId="product.id"
+                       :maxAmount="product.stock"
             ></component>
         </div>
     </div>
 </template>
 
 <script>
-import AddToCart from "../card/add-to-cart.vue"
+import addToCart from "../card/add-to-cart.vue"
 
 export default {
     name:"sticky-card",
@@ -46,11 +57,12 @@ export default {
         }
     },
     components:{
-        AddToCart,
+        addToCart,
     },
     data(){
         return{
-            flag: false
+            showStickyCard: false,
+            showButton: false,
         }
     },
     methods:{
@@ -63,18 +75,26 @@ export default {
                     let windowPosition = window.pageYOffset
 
                     if(windowPosition > productOrderPosition){
-                        this.flag = true
+                        this.showStickyCard = true
                         this.$eventBus.$emit('openStickyCard')
                     }else{
-                        this.flag = false
+                        this.showStickyCard = false
                         this.$eventBus.$emit('closeStickyCard')
                     }
                 }
              }
-        }
+        },
+        sowButton() {
+            this.showButton = true
+        },
+        hideButton() {
+            this.showButton = false
+        },
     },
     created () {
         window.addEventListener('scroll', this.mouseWheel);
+        this.$eventBus.$on("openStickyButton", this.sowButton);
+        this.$eventBus.$on("closeStickyButton", this.hideButton);
     },
 }
 </script>
--- src/js/components/sticky-mobile-button.vue
+++ src/js/components/sticky-mobile-button.vue
@@ -1,32 +1,14 @@
 <template>
     <div class="sticky-mobile-button" v-if="showButton">
-        <a v-if="flag" href="" class="sticky-mobile-button__button">{{ titlePartFirst }} <span>{{ items }}&nbsp;{{ titlePartSecond }}</span></a>
-        <component v-else is="add-to-cart" 
-                   :text="'В корзине'"
-                   :button-title="'Добавить в корзину'"
-                   :size="'big'"
-                   :productId="productId"
-                   :maxAmount="maxAmount"
-        ></component>
+        <a href="" class="sticky-mobile-button__button">{{ titlePartFirst }} <span>{{ items }}&nbsp;{{ titlePartSecond }}</span></a>
     </div>
     
 </template>
 
 <script>
-import addToCart from './card/add-to-cart.vue'
-
 export default {
     name: "sticky-mobile-button",
-    components: {
-        addToCart
-    },
     props: {
-        productId: {
-            type: Number,
-        },
-        maxAmount: {
-            type: Number,
-        },
     },
     data(){
         return{
@@ -34,22 +16,14 @@ export default {
             items: 0,
             titlePartFirst: '',
             titlePartSecond: '',
-            flag: true
         }
     },
     created() {
         this.$eventBus.$on("sow-button", this.sowButton);
         this.$eventBus.$on("hide-button", this.hideButton);
-        this.$eventBus.$on("openStickyButton", this.sowButton);
-        this.$eventBus.$on("closeStickyButton", this.hideButton);
     },
     methods:{
         sowButton(firstPart, items, secondPart) {
-            if(items || secondPart){
-                this.flag = true
-            }else{
-                this.flag = false
-            }
             this.showButton = true
             this.items = items
             this.titlePartFirst = firstPart
