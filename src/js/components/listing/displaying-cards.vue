816e3a1e29bb478e54c9b43db79593fca3688fd5
Yakovleva Irina <yakovleva@instrumtorg.ru>
2021-01-12 15:47:22 +0300

    filter actions: displaying grid or row

--- src/js/components/listing/displaying-cards.vue
+++ src/js/components/listing/displaying-cards.vue
@@ -1,13 +1,41 @@
 <template>
     <div class="displaying">
-        <div class="displaying__button"
-             :class="{'displaying__button--active': activeDisplaying === 'grid'}"
-             @click="changeDisplaying('list')"
-        >grid</div>
-        <div class="displaying__button"
-             :class="{'displaying__button--active': activeDisplaying === 'list'}"
-             @click="changeDisplaying('grid')"
-        >list</div>
+        <div class="displaying__block displaying__block_mobile">
+            <div class="displaying__button"
+                :class="{'displaying__button--active': activeDisplaying === 'grid'}"
+                @click="changeDisplaying('list')"
+            >
+                <svg class="displaying__icon displaying__icon_grid">
+                    <use :xlink:href="templatePath + 'images/sprite.svg#icons__view-grid'"></use>
+                </svg>
+            </div>
+            <div class="displaying__button"
+                :class="{'displaying__button--active': activeDisplaying === 'list'}"
+                @click="changeDisplaying('grid')"
+            >
+                <svg class="displaying__icon displaying__icon_rows">
+                    <use :xlink:href="templatePath + 'images/sprite.svg#icons__view-rows'"></use>
+                </svg>
+            </div>
+        </div>
+        <div class="displaying__block displaying__block_desktop">
+            <div class="displaying__button"
+                :class="{'displaying__button--active': activeDisplaying === 'grid'}"
+                @click="changeDisplaying('grid')"
+            >
+                <svg class="displaying__icon displaying__icon_grid">
+                    <use :xlink:href="templatePath + 'images/sprite.svg#icons__view-grid'"></use>
+                </svg>
+            </div>
+            <div class="displaying__button"
+                :class="{'displaying__button--active': activeDisplaying === 'list'}"
+                @click="changeDisplaying('list')"
+            >
+                <svg class="displaying__icon displaying__icon_rows">
+                    <use :xlink:href="templatePath + 'images/sprite.svg#icons__view-rows'"></use>
+                </svg>
+            </div>
+        </div>
     </div>
 </template>
 
