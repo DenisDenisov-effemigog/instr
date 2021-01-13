48d96cc0754d02ee356c03a503a20a8fa8ae8f47
Denis <effemigog@yandex.ru>
2021-01-13 17:41:21 +0300

    modal filter

--- src/js/components/modal.vue
+++ src/js/components/modal.vue
@@ -50,7 +50,15 @@
                 <component is="user" :selected="props"></component>
             </div>
             <div class="modal_filters" v-else-if="modal === 'filters'">
-                <h3 class="modal-title">Фильтры</h3><!--TODO сделать стики хэдер у модалки-->
+                <div class="modal_filters-sticky">
+                    <div class="modal_filters-sticky__icon" @click="closeModal">
+                        <svg>
+                            <use :xlink:href="templatePath + 'images/sprite.svg#arrows__arrow-left'"></use>
+                        </svg>
+                    </div>
+                    <h3 class="modal-title">Фильтры</h3><!--TODO сделать стики хэдер у модалки-->
+                    <filter-clear-btn :mobileFlag=true></filter-clear-btn>
+                </div>
                 <slot name="listingcat"></slot>
                 <!--TODO добавить странную кнопку "все категории"-->
                 <component is="filters" :filters="props"></component>
@@ -81,6 +89,7 @@
     import filters from "./listing/filters/filters.vue"
     import stickyMobileButton from "./sticky-mobile-button.vue"
     import ClickOutside from "vue-click-outside"
+import FilterClearBtn from './listing/filters/filter-clear-btn.vue'
 
 export default {
     name: 'modal',
@@ -92,6 +101,7 @@ export default {
         addAddress,
         filters,
         stickyMobileButton,
+        FilterClearBtn
     },
     directives: {
         ClickOutside
