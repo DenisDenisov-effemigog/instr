df2b95ec214c588657dea35081d5bd0d0e6279fd
Yakovleva Irina <yakovleva@instrumtorg.ru>
2021-01-15 17:52:59 +0300

    cart page layout

--- src/js/components/cart/cart.vue
+++ src/js/components/cart/cart.vue
@@ -1,6 +1,42 @@
 <template>
-    <div class="cart">Реализовать сначала корзину без товаров, потом добавить условие и сверстать табличный вариант корзины
-    </div>
+    <section class="cart">
+        <div class="cart__content">
+            <div class="cart__purchases-wrapper">
+                <div class="cart__border-right">
+                    <div class="cart__header">
+                        <div class="cart__title">Корзина</div>
+                        <div class="cart__header-layout">
+                            <div class="cart__view-switcher">
+                                <label name="mode" class="cart__view-switch">
+                                    <input
+                                        class="cart__view-switch-input"
+                                        type="checkbox"
+                                        name="mode">
+                                    <span class="cart__view-switch-slider"></span>
+                                    <span class="cart__view-switch-label">Табличный&nbsp;режим</span>
+                                </label>
+                            </div>
+                            <div class="cart__clear-btn">
+                                <svg>
+                                    <use :xlink:href="templatePath + 'images/sprite.svg#icons__delete'"></use>
+                                </svg>
+                                Очистить корзину
+                            </div>
+                        </div>
+                    </div>
+                    <div class="cart__purchases">
+                        Карточки товаров для корзины
+                    </div>
+                </div>
+            </div>
+            <div class="cart__sidebar-wrapper">
+                <div class="cart__sidebar">
+                    Кнопка оформления заказов
+                </div>
+
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -16,6 +52,7 @@
         },
         data(){
             return{
+                
             }
         },
         methods:{
