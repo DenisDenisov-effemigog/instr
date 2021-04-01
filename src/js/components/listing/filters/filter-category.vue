1a6e08be05f3ca07f26aa8588443d845a24a3c35
Yakovleva Irina <yakovleva@instrumtorg.ru>
2021-04-01 16:27:36 +0300

    listing category is not filter, margin fixing

--- src/js/components/listing/filters/filter-category.vue
+++ src/js/components/listing/filters/filter-category.vue
@@ -2,18 +2,26 @@
     <ul class="listing__further-subcategories">
         <slot v-if="!subcategoryShowAll" name="subcategory-listing-part"></slot>
         <slot v-else-if="subcategoryShowAll" name="subcategory-listing-all"></slot>
-        <li v-if="!subcategoryShowAll" class="listing__subcategory-btn">
-            <a href="" @click.prevent="subcategoryShowAll = true">{{ $tc('header.catalogue.show_more') }}</a>
-        </li>
-        <li v-else-if="subcategoryShowAll" class="listing__subcategory-btn">
-            <a href="" @click.prevent="subcategoryShowAll = false">{{ $tc('header.catalogue.hide_more') }}</a>
-        </li>
+        <template v-if="showButton">
+            <li v-if="!subcategoryShowAll" class="listing__subcategory-btn">
+                <a href="" @click.prevent="subcategoryShowAll = true">{{ $tc('header.catalogue.show_more') }}</a>
+            </li>
+            <li v-else-if="subcategoryShowAll" class="listing__subcategory-btn">
+                <a href="" @click.prevent="subcategoryShowAll = false">{{ $tc('header.catalogue.hide_more') }}</a>
+            </li>
+        </template>
     </ul>
 </template>
 
 <script>
     export default {
         name: "filter-category",
+        props: {
+            showButton: {
+                type: Boolean,
+                default: true
+            }
+        },
         data(){
             return{
                 subcategoryShowAll: false,
