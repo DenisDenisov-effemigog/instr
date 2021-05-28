26ca28e4f35f7cc2000898d8010ffb46776ba0f6
Denis <effemigog@yandex.ru>
2021-05-28 13:49:56 +0300

    fix

--- src/js/components/profile/page-modals/edit-profile.vue
+++ src/js/components/profile/page-modals/edit-profile.vue
@@ -176,9 +176,9 @@
             saveChanges() {
                 let vm = this
                 
-                api.editProfile(vm.$v.name.$model, vm.$v.phone.$model, vm.$v.email.$model).then(answer => {
+                api.editProfile(vm.$v.name.$model, vm.$v.phone.$model, vm.$v.email.$model).then((data) => {
                     vm.$eventBus.$emit('closeModal')
-                    vm.$eventBus.$emit('editProfile', answer)
+                    vm.$eventBus.$emit('editProfile', data)
                 }).catch(errors => {
                     console.error(errors);
                 })
